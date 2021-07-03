21016d5a0c9ac18a4ca9bf07dd6be74838412feb
Reinoptland <reinoptland@gmail.com>
2021-07-03 13:13:49 +0200

    Write install guide, add script to generate secret

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const projectRouter = require("./routers/projects")
-const userRouter = require("./routers/users")
-const webhookRouter = require("./routers/webhooks")
-const loginRouter = require("./routers/login")
-const adminRouter = require("./routers/admin")
-const { awsDecodedSecret } = require("./src/secrets")
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const projectRouter = require("./routers/projects");
+const userRouter = require("./routers/users");
+const webhookRouter = require("./routers/webhooks");
+const loginRouter = require("./routers/login");
+const adminRouter = require("./routers/admin");
+const { awsDecodedSecret } = require("./src/secrets");
 
 const secretsFromAws = async () => {
-  const secret = await awsDecodedSecret()
-  console.log(secret)
-  return secret
-}
+  const secret = await awsDecodedSecret();
+  console.log(secret);
+  return secret;
+};
 
-secretsFromAws()
+// secretsFromAws()
 
-app.set("view engine", "ejs")
-app.use(express.json())
+app.set("view engine", "ejs");
+app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true,
   })
-)
-app.use(cors())
+);
+app.use(cors());
 
-app.use("/projects", projectRouter)
-app.use("/users", userRouter)
-app.use("/webhooks", webhookRouter)
-app.use("/login", loginRouter)
-app.use("/admin", adminRouter)
+app.use("/projects", projectRouter);
+app.use("/users", userRouter);
+app.use("/webhooks", webhookRouter);
+app.use("/login", loginRouter);
+app.use("/admin", adminRouter);
 
-module.exports = app
+module.exports = app;
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const app = require("./app")
-const { sequelize } = require("./models")
+const app = require("./app");
+const { sequelize } = require("./models");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, async () => {
-  console.log(`Listening on port ${PORT}`)
-  await sequelize.authenticate()
-  console.log("Database connected!")
-})
+  console.log(`Listening on port ${PORT}`);
+  await sequelize.authenticate();
+  console.log("Database connected!");
+});
